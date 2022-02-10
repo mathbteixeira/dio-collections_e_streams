625f486c72c0a9016b83578412aeffc55ec29fea
Matheus Teixeira <mathbteixeira@gmail.com>
2022-02-10 10:58:24 -0200

    Métodos HashMap - part 1

---
+++ src/main/java/one/digitalinnovation/map/ExemploHashMap.java
@@ -0,0 +1,23 @@
+package src.main.java.one.digitalinnovation.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExemploHashMap {
+    public static void main(String[] args) {
+        Map<String, Integer> campeoesMundialFifa = new HashMap<>();
+
+        campeoesMundialFifa.put("Brasil", 5);
+        campeoesMundialFifa.put("Alemanha", 4);
+        campeoesMundialFifa.put("Italia", 4);
+        campeoesMundialFifa.put("Uruguai", 2);
+        campeoesMundialFifa.put("Argentina", 2);
+        campeoesMundialFifa.put("França", 2);
+        campeoesMundialFifa.put("Inglaterra", 1);
+        campeoesMundialFifa.put("Espanha", 1);
+
+        System.out.println(campeoesMundialFifa);
+
+        
+    }
+}
